a6c6f1a82961255cee282b6ccb39146dfa23856f
Nic Aitch <nic@nicinabox.com>
2015-04-01 10:16:29 -0500

    Externalize dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,16 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './dist/calendar.js'
+    filename: './dist/calendar.js',
+     libraryTarget: "var",
+     library: "Calendar"
+  },
+  externals: {
+    "lodash": "_",
+    "jquery": "$"
   },
   module: {
     loaders: [
-      {
-        test: require.resolve("./src/index"),
-        loader: "expose?Calendar"
-      },
       {
         test: /\.html$/,
         loader: path.join(__dirname, 'loaders/html-template-loader.js')
