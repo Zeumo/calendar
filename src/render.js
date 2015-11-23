cf2ece8e13cd76e7124ff795cbbe17a1cc8c0276
Nic Aitch <nic@nicinabox.com>
2015-11-23 16:31:06 -0600

    Fix parsing string dates

--- src/render.js
+++ src/render.js
@@ -1,8 +1,21 @@
 var builder = require('./builder');
 
+var parseDates = function(events) {
+  return _.map(events, function(event) {
+    if (event.start_date instanceof Date) {
+      return event
+    } else {
+      return _.merge(event, {
+        start_date: new Date(event.start_date),
+        end_date: new Date(event.end_date)
+      });
+    }
+  });
+}
+
 module.exports = function (newDate, calendarEvents) {
   this.currentDate = newDate || this.currentDate;
-  this.calendarEvents = calendarEvents || this.calendarEvents;
+  this.calendarEvents = parseDates(calendarEvents || this.calendarEvents);
 
   this.el.innerHTML = builder.template({
     date: this.currentDate,
